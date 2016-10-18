7c61bbc926373582a35270345759ce998229fd83
Troy <tward4@tulane.edu>
2016-10-18 11:40:42 -0400

    Merge branch 'master' of https://github.com/TIY-Charleston-Back-End-Sept2016/IronGram
    # Conflicts:
    #	src/com/theironyard/Main.java
    Initial Login system again

--- src/com/theironyard/User.java
+++ src/com/theironyard/User.java
@@ -4,4 +4,19 @@ package com.theironyard;
  * Created by Troy on 10/18/16.
  */
 public class User {
+    int id;
+    String name;
+
+    public User(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
