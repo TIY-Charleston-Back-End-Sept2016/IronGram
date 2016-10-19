c3038231807d155f28dfed05a205106ebf484b23
michaelplott <mike.plott80@gmail.com>
2016-10-19 10:11:39 -0400

    Merge branch 'master' of https://github.com/TIY-Charleston-Back-End-Sept2016/IronGram
    # Conflicts:
    #	public/index.html
    #	public/main.js
    #	src/com/theironyard/Image.java
    #	src/com/theironyard/Main.java
    #	src/com/theironyard/User.java

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -36,16 +36,27 @@ public class Main {
         return null;
     }
 
-    static void insertImage(Connection conn, String filename, int userId) throws SQLException {
-        PreparedStatement stmt = conn.prepareStatement("INSERT INTO images VALUES (NULL, ?, ?)");
+    static int insertImage(Connection conn, String filename, int userId) throws SQLException {
+        PreparedStatement stmt = conn.prepareStatement("INSERT INTO images VALUES (NULL, ?, ?)", Statement.RETURN_GENERATED_KEYS);
         stmt.setString(1, filename);
         stmt.setInt(2, userId);
         stmt.execute();
+
+        ResultSet rs = stmt.getGeneratedKeys();
+        if (rs.next()) {
+            return rs.getInt(1);
+        }
+        return 0;
     }
 
-    static ArrayList<Image> selectImages(Connection conn) throws SQLException {
+    static ArrayList<Image> selectImages(Connection conn, int userID) throws SQLException {
         ArrayList<Image> images = new ArrayList<>();
-        PreparedStatement stmt = conn.prepareStatement("SELECT * FROM images INNER JOIN users ON images.user_id = users.id");
+        PreparedStatement stmt = conn.prepareStatement(
+                "SELECT * FROM images INNER JOIN users ON images.user_id = users.id " +
+                        "INNER JOIN recipients ON images.id = recipients.image_id WHERE recipients.user_id = ? OR images.user_id = ?"
+        );
+        stmt.setInt(1, userID);
+        stmt.setInt(2, userID);
         ResultSet results = stmt.executeQuery();
         while (results.next()) {
             int id = results.getInt("images.id");
@@ -57,8 +68,15 @@ public class Main {
         return images;
     }
 
+    static void insertRecipient(Connection conn, int userID, int imageID) throws SQLException {
+        PreparedStatement stmt = conn.prepareStatement("INSERT INTO recipients VALUES (NULL, ?, ?)");
+        stmt.setInt(1, userID);
+        stmt.setInt(2, imageID);
+        stmt.execute();
+    }
+
     public static void main(String[] args) throws SQLException {
-        Connection conn = DriverManager.getConnection("jdbc:h2:./main");
+        Connection conn = DriverManager.getConnection("jdbc:h2:./man");
         createTables(conn);
 
         Spark.externalStaticFileLocation("public");
@@ -121,7 +139,17 @@ public class Main {
 
                         Session session = request.session();
                         User user = selectUser(conn, session.attribute("username"));
-                        insertImage(conn, f.getName(), user.id);
+                        int imageID = insertImage(conn, f.getName(), user.id);
+                        insertRecipient(conn, user.id, imageID);
+
+                        String recipients = request.queryParams("recipients");
+                        String[] recArray = recipients.split(",");
+                        for (String rec : recArray) {
+                            User recUser = selectUser(conn, rec);
+                            if (recUser != null) {
+                                insertRecipient(conn, recUser.id, imageID);
+                            }
+                        }
                     }
                     response.redirect("/");
                     return null;
@@ -131,8 +159,14 @@ public class Main {
         Spark.get(
                 "/images",
                 (request, response) -> {
+                    Session session = request.session();
+                    String name = session.attribute("username");
+                    User user = selectUser(conn, name);
+                    if (user == null) {
+                        return "[]";
+                    }
                     JsonSerializer serializer = new JsonSerializer();
-                    return serializer.serialize(selectImages(conn));
+                    return serializer.serialize(selectImages(conn, user.id));
                 }
         );
     }
